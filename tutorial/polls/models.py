270fd2b0215735f43a802bd4072f51b31853a137
Kangbok Lee <gangbok119@gmail.com>
2017-09-28 17:29:38 +0900

    datetime 모듈, timedelta 활용법

--- tutorial/polls/models.py
+++ tutorial/polls/models.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from django.db import models
 
 # Create your models here.
@@ -24,17 +26,22 @@ class Choice
         설문조사 {{설문조사의 제목}} - 선택지 {{제목}}
 '''
 
+
 class Question(models.Model):
-    title =models.CharField(max_length=50)
-    published_date = models.DateTimeField(blank=True,null=True)
+    title = models.CharField(max_length=50)
+    published_date = models.DateTimeField(blank=True, null=True)
 
-
+    def is_recently(self):
+        return bool(self.published_date) and timezone.now() - self.published_date <= timedelta(days=7)
+# None은 None이므로 bool값을 반환받으려면 bool()로 감싸자...
     def __str__(self):
         return ('설문조사 : {}'.format(self.title))
 
+
 class Choice(models.Model):
-    question = models.ForeignKey(Question)#Question 클래스가 여러 Choice와 연결 다대일 관계
-    title= models.CharField(max_length=50)
+    question = models.ForeignKey(Question)  # Question 클래스가 여러 Choice와 연결 다대일 관계
+    title = models.CharField(max_length=50)
     vote = models.IntegerField(default=0)
+
     def __str__(self):
-        return ('{} - 선택지 : {}'.format(self.question,self.title))
+        return ('{} - 선택지 : {}'.format(self.question, self.title))

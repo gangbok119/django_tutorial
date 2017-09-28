00607917e88ecc993e67fa36b584e4325e313583
Kangbok Lee <gangbok119@gmail.com>
2017-09-28 16:20:42 +0900

    Question/Choice 모델 생성, admin에 등록

--- tutorial/config/settings.py
+++ tutorial/config/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+TEMPLATES_DIR = os.path.join('templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+
+    'polls',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +106,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
---
+++ tutorial/polls/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+# Create your models here.
+from django.utils import timezone
+
+'''
+class Question
+    설문조사 질문
+    title = 설문조사 제목
+    published_date =
+
+    __str__
+    print(Question instance)
+        설문좌 {{ 제목 }}
+
+class Choice
+    -> 선택
+    question = 해당 설문조사
+    title = 선택지 제목
+    votes = 선택 횟수  = (IntegerField)
+
+    __str__
+    print(Choice instance)
+        설문조사 {{설문조사의 제목}} - 선택지 {{제목}}
+'''
+
+class Question(models.Model):
+    title =models.CharField(max_length=50)
+    published_date = models.DateTimeField(blank=True,null=True)
+
+
+    def __str__(self):
+        return ('설문조사 {}'.format(self.title))
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)#Question 클래스가 여러 Choice와 연결 다대일 관계
+    title= models.CharField(max_length=50)
+    vote = models.IntegerField(default=0)
+    def __str__(self):
+        return ('설문조사 {} - 선택지 {}'.format(self.question,self.title))
